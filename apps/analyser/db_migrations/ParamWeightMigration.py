198885a1741229533a24426a9496a55741c06d45
xboct <xboctatbiii@gmail.com>
2023-10-30 00:16:12 +0300

    [BF] fixed insert of default weights

--- apps/analyser/db_migrations/ParamWeightMigration.py
+++ apps/analyser/db_migrations/ParamWeightMigration.py
@@ -1,15 +1,20 @@
-from sqlalchemy import Connection
+from sqlalchemy import Connection, select
 from sqlalchemy.dialects.postgresql import insert
 
-from apps.analyser.db_migrations.dictionaries import migration_dictionaries
+from apps.analyser.models.db.CleanDataParamsDictionaryDto import CleanDataParamsDictionaryDto
 from apps.analyser.models.db.ParamWeightDto import ParamWeightDto
 
 
 def insert_weights(connection: Connection):
+    ids_select_statement = select(CleanDataParamsDictionaryDto)
+    ids_select_db_result = connection.execute(ids_select_statement).all()
+    ids = list(map(lambda x: [x[0], x[1]], ids_select_db_result))
+
     values_to_insert = list(map(
-        lambda x: {'param_name': x, 'weight_value': 0},
-        migration_dictionaries['clean_data_params_dictionary']
+        lambda x: {'param_name': x[0], 'weight_value': 0},
+        ids
     ))
+
     insert_params_statement = insert(ParamWeightDto)\
         .values(values_to_insert)\
         .on_conflict_do_nothing()
