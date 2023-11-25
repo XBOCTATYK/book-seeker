ca32f53de90f8d71a95688cf282b81908cd9ae53
a.startsev <a.startsev@qiwi.com>
2023-11-25 22:45:25 +0300

    [CF] added needed services

--- apps/notifier/NotifierApp.py
+++ apps/notifier/NotifierApp.py
@@ -7,6 +7,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, Application
 from apps.AbstractApp import AbstractApp
 from apps.notifier.handlers.TelegramHandler import TelegramHandler
 from apps.notifier.handlers.StartHandlers import StartHandler
+from apps.notifier.repositories.TgUserToFetchOptionsRepository import TgUserToFetchOptionsRepository
+from apps.notifier.services.TgUsersNotifier import TgUsersNotifier
 from apps.transit_data_app.repositories.FilteredDataRepository import FilteredDataRepository
 from common.services.OffsetPointerRepository import OffsetPointerRepository
 from datasource.DbLikeDataSource import DbLikeDataSource
@@ -20,8 +22,12 @@ class NotifierApp(AbstractApp):
     _offset_pointer_notifier: OffsetPointerRepository
     _offset_pointer_notifier_name = 'notifier_app'
     _filtered_data_repository: FilteredDataRepository
+    _tg_user_to_fetch_options_repository: TgUserToFetchOptionsRepository
+    _tg_user_to_fetch_offset_pointer: OffsetPointerRepository
+    _tg_user_to_fetch_offset_pointer_name = 'tg_user_to_fetch'
     _telegram_application: Application
     _scheduler: BackgroundScheduler
+    _tg_users_notifier: TgUsersNotifier
 
     def __init__(self, config: dict):
         super().__init__(config)
@@ -35,6 +41,15 @@ class NotifierApp(AbstractApp):
         self._data_source = DbLikeDataSource(db_config, 'notifier_app_data_source')
         self._offset_pointer_notifier = OffsetPointerRepository(self._data_source, self._offset_pointer_notifier_name)
         self._filtered_data_repository = FilteredDataRepository(self._data_source, self._offset_pointer_notifier)
+        self._tg_user_to_fetch_offset_pointer = OffsetPointerRepository(
+            self._data_source,
+            self._tg_user_to_fetch_offset_pointer_name
+        )
+        self._tg_user_to_fetch_options_repository = TgUserToFetchOptionsRepository(
+            self._data_source,
+            self._tg_user_to_fetch_offset_pointer
+        )
+        self._tg_users_notifier = TgUsersNotifier(self._tg_user_to_fetch_options_repository)
 
         self._telegram_application = ApplicationBuilder().token(bot_config['token']).build()
 
@@ -53,9 +68,10 @@ class NotifierApp(AbstractApp):
 
     def _job(self):
         bot: Bot = self._telegram_application.bot
+
         self._filtered_data_repository.process_next_n(
             10,
-            lambda chat_id, message: bot.send_message(chat_id=chat_id, text=message)
+            lambda chat_id, message: self._tg_users_notifier.notify(bot)
         )
 
     def _run_schedulers(self):
