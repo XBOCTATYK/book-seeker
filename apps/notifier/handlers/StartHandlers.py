8250388cb8e73ac9ee0b995e6871f880f565fe3d
xboct <xboctatbiii@gmail.com>
2023-12-25 23:56:40 +0300

    [СF] changed start description

--- apps/notifier/handlers/StartHandlers.py
+++ apps/notifier/handlers/StartHandlers.py
@@ -6,7 +6,7 @@ from apps.notifier.handlers.TelegramHandler import TelegramHandler
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.effective_chat.id)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="You have just activated me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Now you can use /taskadd command to add new url to discover!")
 
 
 class StartHandler(TelegramHandler):
